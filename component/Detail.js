d98c0e9c9c25dbe5f5684e964d906836bdebdf79
kali1402 <amcopeew68@naver.com>
2020-09-25 10:05:07 +0900

    디테일 페이지 추가

--- component/Detail.js
+++ component/Detail.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Container, Header, Content, Card, CardItem, Text, Body, Left, Icon, Button } from 'native-base';
 import { StyleSheet } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import Map from './Map';
 
 function Detail({ job }) {
 
@@ -9,18 +10,16 @@ function Detail({ job }) {
     const moveList = () => {
         Actions.pop();
     };
-
     return (
         <Container>
             <Header>
                 <Left style={{ flexDirection: 'row', alignItems: 'center' }}>
                     <Button onPress={moveList} transparent>
                         <Icon name="arrow-back" style={{ marginRight: 10 }} />
-                        <Text>List</Text>
+                        <Text>목록</Text>
                     </Button>
                 </Left>
                 <Body>
-                    <Text>???</Text>
                 </Body>
             </Header>
             <Content>
@@ -38,7 +37,7 @@ function Detail({ job }) {
                         </Body>
                     </CardItem>
                     <CardItem footer>
-                        <Text>Footer</Text>
+                        <Maps />
                     </CardItem>
                 </Card>
             </Content>
